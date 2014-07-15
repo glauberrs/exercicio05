5a72da63206672ade92500286b18d74e9c26487a
Jackson F. de A. Mafra <jacksonfdam@gmail.com>
2014-07-15 17:42:50 -0300

    Initial Commit

---
+++ exercicio05Tests/exercicio05Tests.m
@@ -0,0 +1,34 @@
+//
+//  exercicio05Tests.m
+//  exercicio05Tests
+//
+//  Created by Jackson F. de A. Mafra on 15/07/2014.
+//  Copyright (c) 2014 Jackson F. de A. Mafra. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface exercicio05Tests : XCTestCase
+
+@end
+
+@implementation exercicio05Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
